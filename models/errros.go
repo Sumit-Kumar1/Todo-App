4860c3c02007b8ebdd29f1ea4151487df45f2050
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-07-19 13:21:46 +0530

    Added nolint for unused methods

--- models/errros.go
+++ models/errros.go
@@ -8,7 +8,7 @@ import (
 // Errors raised by package models.
 var (
 	ErrPermissionDenied = constError("permission denied")
-	ErrInvalidId        = constError("invalid id")
+	ErrInvalidID        = constError("invalid id")
 	ErrNotFound         = constError("not found")
 	ErrTaskTitle        = constError("invalid task title")
 	ErrTaskDone         = constError("invalid task done")
@@ -19,29 +19,38 @@ type constError string
 func (err constError) Error() string {
 	return string(err)
 }
+
 func (err constError) Is(target error) bool {
 	ts := target.Error()
 	es := string(err)
 	return ts == es || strings.HasPrefix(ts, es+": ")
 }
+
+//nolint:unused // will be using it
 func (err constError) wrap(inner error) error {
 	return wrapError{msg: string(err), err: inner}
 }
 
+//nolint:unused // will be using it
 type wrapError struct {
 	err error
 	msg string
 }
 
+//nolint:unused // will be using it
 func (err wrapError) Error() string {
 	if err.err != nil {
 		return fmt.Sprintf("%s: %v", err.msg, err.err)
 	}
 	return err.msg
 }
+
+//nolint:unused // will be using it
 func (err wrapError) Unwrap() error {
 	return err.err
 }
+
+//nolint:unused // will be using it
 func (err wrapError) Is(target error) bool {
 	return constError(err.msg).Is(target)
 }
