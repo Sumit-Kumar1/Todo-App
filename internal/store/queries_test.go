4436ff316d224a5b691ab6027b3bd0b1a5eff5ef
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-08-07 12:00:45 +0530

    Added tests and drag and drop functionality for tasks

--- internal/store/queries_test.go
+++ internal/store/queries_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 func Test_genInsertQuery(t *testing.T) {
+	ts := time.Now()
+	query := "INSERT INTO tasks (task_id, task_title, done_status, added_at) VALUES (?, ?, ?, ?);"
 	tests := []struct {
 		name       string
 		id         string
@@ -16,7 +18,7 @@ func Test_genInsertQuery(t *testing.T) {
 		wantQuery  string
 		wantValues []any
 	}{
-		// TODO: Add test cases.
+		{name: "valid case", id: "abcde", title: "Dog Walk", ts: ts, wantQuery: query, wantValues: []any{"abcde", "Dog Walk", 0, ts}},
 	}
 	for i, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -24,6 +26,30 @@ func Test_genInsertQuery(t *testing.T) {
 
 			assert.Equalf(t, tt.wantQuery, gotQuery, "TEST[%d] Failed - %s", i, tt.name)
 			assert.Equalf(t, tt.wantValues, gotValues, "TEST[%d] Failed - %s", i, tt.name)
+		})
+	}
+}
+
+func Test_genUpdateQuery(t *testing.T) {
+	ts := time.Now()
+	query := "UPDATE tasks SET task_title=?, done_status=?, modified_at=? WHERE task_id=?;"
+
+	tests := []struct {
+		name      string
+		id        string
+		title     string
+		ts        time.Time
+		wantQuery string
+		wantVals  []any
+	}{
+		{name: "valid case", id: "abcde", title: "Dog Walk", ts: ts, wantQuery: query, wantVals: []any{"Dog Walk", 0, ts, "abcde"}},
+	}
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotQuery, gotVals := genUpdateQuery(tt.id, tt.title, tt.ts)
+
+			assert.Equalf(t, tt.wantQuery, gotQuery, "TEST[%d] Failed - %s", i, tt.name)
+			assert.Equalf(t, tt.wantVals, gotVals, "TEST[%d] Failed - %s", i, tt.name)
 		})
 	}
 }
