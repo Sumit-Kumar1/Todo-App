b6f52046d8586ee46699d9b0fcaeece6499deafa
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-07-13 17:10:29 +0530

    Fixed sql queries

--- internal/store/session/store.go
+++ internal/store/session/store.go
@@ -2,44 +2,38 @@ package sessionstore
 
 import (
 	"context"
-	"fmt"
+	"database/sql"
+	"errors"
 	"log/slog"
-	"time"
 
 	"todoapp/internal/models"
 
 	"github.com/google/uuid"
-	"github.com/sqlitecloud/sqlitecloud-go"
 )
 
 const (
-	createSession      = "INSERT INTO sessions (id, user_id, token, expiry) VALUES ('%v', '%v', '%v','%v');"
-	deleteSessionByID  = "DELETE FROM sessions WHERE id='%v';"
-	getSessionByUserID = "SELECT id, user_id, token, expiry FROM sessions WHERE user_id='%v';"
+	createSession      = "INSERT INTO sessions (id, user_id, token, expiry) VALUES (?, ?, ?,?);"
+	deleteSessionByID  = "DELETE FROM sessions WHERE id=?;"
+	getSessionByUserID = "SELECT id, user_id, token, expiry FROM sessions WHERE user_id=?;"
 	//nolint:gosec //not any hardcoded credential
-	getSessionByToken = "SELECT id FROM sessions where token='%v';"
-	updateSession     = "UPDATE sessions SET token='%v',  expiry='%v' WHERE id='%v';"
+	getSessionByToken = "SELECT id FROM sessions where token=?;"
+	updateSession     = "UPDATE sessions SET token=?, expiry=? WHERE id=?;"
 )
 
 type Store struct {
-	DB *sqlitecloud.SQCloud
+	DB *sql.DB
 }
 
-func New(db *sqlitecloud.SQCloud) *Store {
+func New(db *sql.DB) *Store {
 	return &Store{DB: db}
 }
 
 func (s *Store) CreateSession(ctx context.Context, session *models.SessionData) error {
 	logger := models.GetLoggerFromCtx(ctx)
 
-	query := fmt.Sprintf(
-		createSession,
-		session.ID,
-		session.UserID,
-		session.Token,
-		session.Expiry.UnixMilli(),
-	)
-	if err := s.DB.Execute(query); err != nil {
+	res, err := s.DB.ExecContext(ctx, createSession, session.ID,
+		session.UserID, session.Token, session.Expiry.UnixMilli())
+	if err != nil {
 		logger.LogAttrs(ctx, slog.LevelError, "error while running session create query",
 			slog.String("error", err.Error()),
 		)
@@ -47,89 +41,74 @@ func (s *Store) CreateSession(ctx context.Context, session *models.SessionData) 
 		return err
 	}
 
-	return nil
-}
-
-// nolint:gocognit // cognitive of 11 is okay, didn't want to split this method
-func (s *Store) GetSessionByID(ctx context.Context, userID *uuid.UUID) (*models.SessionData, error) {
-	logger := models.GetLoggerFromCtx(ctx)
-
-	var session models.SessionData
-
-	res, err := s.DB.Select(fmt.Sprintf(getSessionByUserID, *userID))
-	if err != nil {
-		logger.LogAttrs(ctx, slog.LevelError, "error while fetching session by userID",
-			slog.String("error", err.Error()),
-		)
-
-		return nil, err
-	}
-
-	if res.GetNumberOfRows() == uint64(0) {
-		logger.LogAttrs(ctx, slog.LevelError, "no user session found for userID",
-			slog.String("userID", userID.String()),
-		)
-
-		return nil, models.ErrNotFound("user ID")
-	}
-
-	for r := uint64(0); r < res.GetNumberOfRows(); r++ {
-		c1, err := res.GetStringValue(r, 0)
-		if err != nil {
-			return nil, err
-		}
-
-		c2, err := res.GetStringValue(r, 1)
-		if err != nil {
-			return nil, err
-		}
-
-		c3, err := res.GetStringValue(r, 2)
-		if err != nil {
-			return nil, err
-		}
-
-		c4, err := res.GetInt64Value(r, 3)
-		if err != nil {
-			return nil, err
-		}
-
-		session.ID = uuid.MustParse(c1)
-		session.UserID = uuid.MustParse(c2)
-		session.Token = c3
-
-		session.Expiry = time.UnixMilli(c4)
-	}
-
-	return &session, nil
-}
-
-func (s *Store) RefreshSession(ctx context.Context, newSession *models.SessionData) error {
-	logger := models.GetLoggerFromCtx(ctx)
-	query := fmt.Sprintf(updateSession,
-		newSession.Token,
-		newSession.Expiry.UnixMilli(),
-		newSession.ID,
-	)
-
-	if err := s.DB.Execute(query); err != nil {
-		logger.LogAttrs(ctx, slog.LevelError, "error in refreshing session",
-			slog.String("error", err.Error()),
-		)
-
+	if _, err = res.RowsAffected(); err != nil {
 		return err
 	}
 
 	return nil
 }
 
+func (s *Store) GetSessionByID(ctx context.Context, userID *uuid.UUID) (*models.SessionData, error) {
+	logger := models.GetLoggerFromCtx(ctx)
+
+	var session models.SessionData
+
+	res, err := s.DB.QueryContext(ctx, getSessionByUserID, *userID)
+	if err == nil {
+		for res.Next() {
+			if err2 := res.Scan(&session.ID, &session.UserID, &session.Token, &session.Expiry); err2 != nil {
+				return nil, err2
+			}
+		}
+
+		return &session, nil
+	}
+
+	if errors.Is(err, sql.ErrNoRows) {
+		logger.LogAttrs(ctx, slog.LevelError, "store : no user session found for userID",
+			slog.String("userID", userID.String()),
+		)
+
+		return nil, models.ErrNotFound("user ID")
+	}
+
+	logger.LogAttrs(ctx, slog.LevelError, "store : error while fetching session by userID",
+		slog.String("error", err.Error()),
+	)
+
+	return nil, err
+}
+
+func (s *Store) RefreshSession(ctx context.Context, newSession *models.SessionData) error {
+	logger := models.GetLoggerFromCtx(ctx)
+
+	_, err := s.DB.ExecContext(ctx, updateSession, newSession.Token,
+		newSession.Expiry.UnixMilli(), newSession.ID)
+	if err == nil {
+		return nil
+	}
+
+	logger.LogAttrs(ctx, slog.LevelError, "store : error while refreshing session",
+		slog.String("error", err.Error()),
+	)
+
+	return err
+}
+
 func (s *Store) Logout(ctx context.Context, token *uuid.UUID) error {
 	logger := models.GetLoggerFromCtx(ctx)
 
-	var id uuid.UUID
+	var (
+		id uuid.UUID
+		r1 string
+	)
 
-	res, err := s.DB.Select(fmt.Sprintf(getSessionByToken, *token))
-	if err != nil {
+	res := s.DB.QueryRowContext(ctx, getSessionByToken, *token)
+	if err := res.Scan(&r1); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.ErrNotFound("session with current user")
+		}
+
 		logger.LogAttrs(ctx, slog.LevelError, "error while logging out user",
 			slog.String("error", err.Error()),
 		)
@@ -137,18 +116,9 @@ func (s *Store) Logout(ctx context.Context, token *uuid.UUID) error {
 		return err
 	}
 
-	if res.GetNumberOfRows() == 0 {
-		return models.ErrNotFound("session with current user")
-	}
+	id = uuid.MustParse(r1)
 
-	for r := uint64(0); r < res.GetNumberOfRows(); r++ {
-		r1, err := res.GetStringValue(r, 0)
-		if err != nil {
-			return err
-		}
+	_, err := s.DB.ExecContext(ctx, deleteSessionByID, id)
 
-		id = uuid.MustParse(r1)
-	}
-
-	return s.DB.Execute(fmt.Sprintf(deleteSessionByID, id))
+	return err
 }
