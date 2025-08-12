b7f3ad8c65f362f15147688adea203795d2492a7
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-08-12 20:13:38 +0530

    FE:changed border-radius, moved html scripts to public, added btn to toggle password visibility

---
+++ public/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("htmx:responseError", (evt) => {
+    console.log(evt);
+    document.getElementById("errors").innerHTML = evt.detail.xhr.response;
+});
+
+function updateModal(id) {
+    let updateId = "update_" + id
+    let updates = document.getElementById(updateId)
+    updates.showModal()
+}
+
+function togglePasswordVisibility() {
+    const eyeIcon = document.getElementById('toggleEye');
+    const passwordInput = document.getElementById('password');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    }
+}
