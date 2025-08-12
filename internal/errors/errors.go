a98c65a0a476c715c209e794d6ef4a992ff5ff4a
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-08-12 22:19:43 +0530

    BE: moved errors to errors pkg

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -1,4 +1,4 @@
-package models
+package errors
 
 import (
 	"errors"
@@ -54,8 +54,7 @@ func HandleHTTPError(w http.ResponseWriter, err error, status int) {
 	w.WriteHeader(status)
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, _ = fmt.Fprintf(w, `<div class="toast toast-top toast-end">
-  <div class="alert alert-error"><svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-        </svg><span>%s</span></div></div>`, err.Error())
+	_, _ = fmt.Fprintf(w, `<div class="toast toast-top toast-end" id="notifi">
+  <div class="alert alert-error"><i class="fa-solid fa-xmark" id="toastClost" onclick="removeToast()"></i>
+    </svg><span>%d: %s</span></div></div>`, status, err.Error())
 }
