0cf0e63eaa95a4d1908c83f96b1a5f31746833f5
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-08-28 21:04:10 +0530

    FE: added daisyUI classes and styling with login/register

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,97 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Fa from 'svelte-fa';
+	import { faKey, faEnvelope, faEye, faEyeSlash, faE } from '@fortawesome/free-solid-svg-icons';
+
+	let passwordVisible = $state(false);
+	let isLoginPage = $state(false);
+
+	function togglePasswordVisibility() {
+		passwordVisible = !passwordVisible;
+	}
+
+	function getType() {
+		if (passwordVisible) {
+			return 'text';
+		} else {
+			return 'password';
+		}
+	}
+
+	function changePage() {
+		isLoginPage = !isLoginPage;
+	}
+</script>
+
+<div class="flex min-h-screen items-center justify-center bg-base-200 text-base-content">
+	<div
+		class="card-border overflow-w-hidden card gap-2 border-base-300 bg-base-100 card-xl sm:w-2/3 lg:w-1/2"
+	>
+		<div class="card-title justify-center p-3">
+			<h2 class="mt-5 text-center text-xl font-bold">
+				{#if isLoginPage}
+					Sign in to your account
+				{:else}
+					New User Registration
+				{/if}
+			</h2>
+		</div>
+		<div class="card-body gap-2">
+			<form class="flex flex-col items-center justify-center gap-4">
+				<label class="input w-full">
+					<Fa icon={faEnvelope}></Fa>
+					<input
+						id="email"
+						name="email"
+						type="email"
+						autocomplete="email"
+						required
+						class="grow"
+						placeholder="e-mail"
+					/>
+				</label>
+				<div class="w-full">
+					<label class="input w-full">
+						<Fa icon={faKey}></Fa>
+						<input
+							id="password"
+							name="password"
+							type={getType()}
+							required
+							class="w-full grow"
+							placeholder="password"
+							minlength="8"
+						/>
+						<a href="#password" aria-label="password" onclick={togglePasswordVisibility}>
+							{#if !passwordVisible}
+								<Fa icon={faEye}></Fa>
+							{:else}
+								<Fa icon={faEyeSlash}></Fa>
+							{/if}
+						</a>
+					</label>
+				</div>
+				<button type="submit" class="btn btn-outline btn-primary lg:w-1/3">
+					{#if isLoginPage}
+						Sign in
+					{:else}
+						Register
+					{/if}
+				</button>
+			</form>
+
+			<p class="mt-5 text-center text-sm text-gray-500">
+				{#if isLoginPage}
+					Create new account?
+				{:else}
+					Already registered?
+				{/if}
+				<a
+					href="/"
+					class="leading-6 font-semibold text-base-content hover:text-neutral"
+					onclick={changePage}
+					>{#if isLoginPage}Register{:else}Login{/if}</a
+				>
+			</p>
+		</div>
+	</div>
+</div>
