0d2aafa5e474ec1d76b9bceb344aaef5b2d94d72
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-09-02 23:23:56 +0530

    BE:updated server run login

--- backend/main.go
+++ backend/main.go
@@ -1,68 +1,18 @@
 package main
 
 import (
-	"strconv"
-	"todoapp/client"
-
-	todohttp "todoapp/internal/handler/todo"
-	userhttp "todoapp/internal/handler/user"
-	"todoapp/internal/migrations"
-	todosvc "todoapp/internal/service/todo"
-	usersvc "todoapp/internal/service/user"
-	todostore "todoapp/internal/store"
-
-	"gofr.dev/pkg/gofr"
-	"gofr.dev/pkg/gofr/service"
+	"context"
+	"log/slog"
+	"os"
+	"todoapp/cmd"
 )
 
 func main() {
-	app := gofr.New()
+	ctx := context.Background()
 
-	app.Migrate(migrations.All())
-
-	// jwtOpts := []jwt.ParserOption{
-	// 	jwt.WithAudience("todoapp"),
-	// 	jwt.WithExpirationRequired(),
-	// 	jwt.WithIssuer("auth-rest-api"),
-	// }
-
-	// app.EnableOAuth("http://localhost:9001", 10, jwtOpts...)
-
-	setupTasksRoutes(app)
-	setupUserRoutes(app)
-
-	app.Run()
-}
-
-func setupTasksRoutes(app *gofr.App) {
-	todoStore := todostore.New()
-	todoSvc := todosvc.New(todoStore)
-	todoHTTP := todohttp.New(todoSvc)
-
-	app.POST("/task", todoHTTP.AddTask)
-	app.GET("/tasks", todoHTTP.GetAllTasks)
-	app.PUT("/tasks/{id}", todoHTTP.Update)
-	app.PUT("/task/{id}/done", todoHTTP.Done)
-	app.DELETE("/tasks/{id}/delete", todoHTTP.DeleteTask)
-}
-
-func setupUserRoutes(app *gofr.App) {
-	authURL := app.Config.GetOrDefault("AUTH_URL", "http://localhost:9001")
-	maxRetry, err := strconv.Atoi(app.Config.GetOrDefault("MAX_RETRY", "3"))
-	if err != nil {
-		maxRetry = 3
+	if err := cmd.Run(ctx, os.Stdout, nil); err != nil {
+		slog.LogAttrs(ctx, slog.LevelError, err.Error())
 	}
 
-	retryCfg := &service.RetryConfig{MaxRetries: maxRetry}
-	healthCfg := &service.HealthConfig{HealthEndpoint: "health"}
-
-	httpSvc := service.NewHTTPService(authURL, app.Logger(), app.Metrics(), retryCfg, healthCfg)
-
-	authClient := client.New(httpSvc)
-	userSvc := usersvc.New(authClient)
-	userHTTP := userhttp.New(userSvc)
-
-	app.POST("/register", userHTTP.Register)
-	app.POST("/login", userHTTP.Login)
-	app.POST("/logout", userHTTP.Logout)
+	slog.LogAttrs(ctx, slog.LevelInfo, "server is stopped!!")
 }
