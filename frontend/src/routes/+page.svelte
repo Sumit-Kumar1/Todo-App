81b65b150fa04ebf1253273e4149dfd3fff135cb
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-09-13 09:35:02 +0530

    FE:updated fetch logic

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,22 +1,56 @@
-<script>
+<script lang="ts">
 	import Fa from 'svelte-fa';
 	import { faKey, faEnvelope, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 	let passwordVisible = $state.raw(false);
-	let isLoginPage = $state.raw(false);
+	let isLoginPage = $state.raw(true);
+
+	let email = $state('');
+	let password = $state('');
+	let response = $state('');
 
 	function getType() {
 		if (passwordVisible) {
 			return 'text';
-		} else {
-			return 'password';
+		}
+
+		return 'password';
+	}
+
+	async function postData(event: Event) {
+		event.preventDefault();
+		const url = 'http://localhost:9003/' + (isLoginPage ? 'login' : 'register');
+		const reqData = { email: email, password: password };
+
+		try {
+			const res = await fetch(url, {
+                method: 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify(reqData)
+			});
+
+			if (!res.ok) {
+				throw new Error(`HTTP error! status: ${res.status}`);
+			}
+
+            if (!isLoginPage) {
+                const data = await res.json();
+                response = data.message;
+                return
+            }
+
+            response = "user login successfully";
+            console.log("response for login: ",res);
+		} catch (error) {
+			console.error('Error posting data:', error);
+			response = `Error: ${error}`;
 		}
 	}
 </script>
 
-<div class="flex min-h-screen items-center justify-center bg-base-200 text-base-content">
+<div class="bg-base-200 text-base-content flex min-h-screen items-center justify-center">
 	<div
-		class="card-border overflow-w-hidden card gap-2 border-base-300 bg-base-100 card-xl sm:w-2/3 lg:w-1/2"
+		class="card-border overflow-w-hidden card border-base-300 bg-base-100 card-xl gap-2 sm:w-2/3 lg:w-1/2"
 	>
 		<div class="card-title justify-center p-3">
 			<h2 class="mt-5 text-center text-xl font-bold">
@@ -28,10 +62,11 @@
 			</h2>
 		</div>
 		<div class="card-body gap-2">
-			<form class="flex flex-col items-center justify-center gap-4">
+			<form class="flex flex-col items-center justify-center gap-4" onsubmit={postData}>
 				<label class="input w-full">
 					<Fa icon={faEnvelope}></Fa>
 					<input
+						bind:value={email}
 						id="email"
 						name="email"
 						type="email"
@@ -45,6 +80,7 @@
 					<label class="input w-full">
 						<Fa icon={faKey}></Fa>
 						<input
+							bind:value={password}
 							id="password"
 							name="password"
 							type={getType()}
@@ -85,7 +121,7 @@
 				{/if}
 				<a
 					href="/"
-					class="leading-6 font-semibold text-base-content hover:text-neutral"
+					class="text-base-content hover:text-neutral font-semibold leading-6"
 					onclick={() => {
 						isLoginPage = !isLoginPage;
 					}}
@@ -94,4 +130,8 @@
 			</p>
 		</div>
 	</div>
+
+	{#if response}
+		<p>{response}</p>
+	{/if}
 </div>
