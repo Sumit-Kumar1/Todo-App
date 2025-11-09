97174d4012a78574fafd936e7b31ff6031204f8b
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-11-09 17:32:11 +0530

    FE:enable svelte fallback, added 404 page

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -6,7 +6,11 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		})
+	}
 };
 
 export default config;
